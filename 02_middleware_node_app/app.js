8dfe296a933f2aeb4ae787dafa28bcdc413f2d75
manickavivek <31manick@gmail.com>
2021-07-24 10:04:57 +0530

    - Initial checkin

---
+++ 02_middleware_node_app/app.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var app = express();
+var mongodbUrl = "mongodb://localhost:27017/";
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+app.use(express.json());
+
+app.post('/addProduct', function (req, res) {
+    MongoClient.connect(mongodbUrl, function(err, db) {
+        if (err) throw err;        
+        var dbo = db.db("stepp_db");
+        dbo.collection("stock").insertOne({
+            "model_type": req["body"]["model_type"],
+            "model_number": req["body"]["model_number"],
+            "model_color": req["body"]["model_color"],
+            "available_count": req["body"]["available_count"]
+        }, 
+        function(err, result) {
+            if (err) throw err;
+            if(result["acknowledged"] == true) {
+                res.json({"response": "Product Added!!"});
+            }
+            db.close();
+        });
+    });
+});
+
+app.get('/getProducts', function (req, res) {
+    MongoClient.connect(mongodbUrl, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("stepp_db");
+        dbo.collection("stock").find({}).toArray(function(err, result) {
+            if (err) throw err;
+            res.json({"response": result});
+            db.close();
+        });
+    });
+});
+
+app.post('/addTransaction', function (req, res) {
+    MongoClient.connect(mongodbUrl, function(err, db) {
+        if (err) throw err;        
+        var dbo = db.db("stepp_db");
+        dbo.collection("transactions").insertOne({
+            "model_number": req["body"]["model_number"],
+            "transaction_type": req["body"]["transaction_type"],
+            "transaction_date": req["body"]["transaction_date"],
+            "product_rate": req["body"]["product_rate"],
+            "bill_number": req["body"]["bill_number"],
+            "product_quantity": req["body"]["product_quantity"],
+            "discounts": req["body"]["discounts"],
+            "customization_cost": req["body"]["customization_cost"],
+            "comments": req["body"]["comments"]
+        }, 
+        function(err, result) {
+            if (err) throw err;
+            if(result["acknowledged"] == true) {
+                res.json({"response": "Transaction Updated!!"});
+            }
+            db.close();
+        });
+    });
+});
+
+app.post('/updateProductCount', function (req, res) {
+    MongoClient.connect(mongodbUrl, function(err, db) {
+        if (err) throw err;        
+        var dbo = db.db("stepp_db");
+        dbo.collection("stock").updateOne({'model_number': req["body"]["model_number"]}, { $set: { 'available_count': req["body"]["available_count"]}}, 
+        function(err, result) {
+            if (err) throw err;
+            if(result["acknowledged"] == true) {
+                res.json({"response": "Product Count Updated!!"});
+            }
+            db.close();
+        });
+    });
+});
+
+app.listen(3000, function () {
+  console.log('Stepp app listening on port 3000!');
+});
