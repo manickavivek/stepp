3062a5b6d81a04278ce4da1754180075f59b519a
manickavivek <31manick@gmail.com>
2021-08-03 00:45:25 +0530

    - Commented app.js old APIs and created new APIs based on the new design. - Created stockin page. - Completed partial functionalities on stockin page.

--- 02_middleware_node_app/app.js
+++ 02_middleware_node_app/app.js
@@ -12,49 +12,94 @@ app.use(function(req, res, next) {
 
 app.use(express.json());
 
-app.post('/addProduct', function (req, res) {
+// app.post('/addProduct', function (req, res) {
+//     MongoClient.connect(mongodbUrl, function(err, db) {
+//         if (err) throw err;        
+//         var dbo = db.db("stepp_db");
+//         dbo.collection("stock").insertOne({
+//             "model_type": req["body"]["model_type"],
+//             "model_number": req["body"]["model_number"],
+//             "model_color": req["body"]["model_color"],
+//             "available_count": req["body"]["available_count"]
+//         }, 
+//         function(err, result) {
+//             if (err) throw err;
+//             if(result["acknowledged"] == true) {
+//                 res.json({"response": "Product Added!!"});
+//             }
+//             db.close();
+//         });
+//     });
+// });
+
+// app.get('/getProducts', function (req, res) {
+//     MongoClient.connect(mongodbUrl, function(err, db) {
+//         if (err) throw err;
+//         var dbo = db.db("stepp_db");
+//         dbo.collection("stock").find({}).toArray(function(err, result) {
+//             if (err) throw err;
+//             res.json({"response": result});
+//             db.close();
+//         });
+//     });
+// });
+
+// app.post('/addTransaction', function (req, res) {
+//     MongoClient.connect(mongodbUrl, function(err, db) {
+//         if (err) throw err;        
+//         var dbo = db.db("stepp_db");
+//         dbo.collection("transactions").insertOne({
+//             "model_number": req["body"]["model_number"],
+//             "transaction_type": req["body"]["transaction_type"],
+//             "transaction_date": req["body"]["transaction_date"],
+//             "product_rate": req["body"]["product_rate"],
+//             "bill_number": req["body"]["bill_number"],
+//             "product_quantity": req["body"]["product_quantity"],
+//             "discounts": req["body"]["discounts"],
+//             "customization_cost": req["body"]["customization_cost"],
+//             "comments": req["body"]["comments"]
+//         }, 
+//         function(err, result) {
+//             if (err) throw err;
+//             if(result["acknowledged"] == true) {
+//                 res.json({"response": "Transaction Updated!!"});
+//             }
+//             db.close();
+//         });
+//     });
+// });
+
+// app.post('/updateProductCount', function (req, res) {
+//     MongoClient.connect(mongodbUrl, function(err, db) {
+//         if (err) throw err;        
+//         var dbo = db.db("stepp_db");
+//         dbo.collection("stock").updateOne({'model_number': req["body"]["model_number"]}, { $set: { 'available_count': req["body"]["available_count"]}}, 
+//         function(err, result) {
+//             if (err) throw err;
+//             if(result["acknowledged"] == true) {
+//                 res.json({"response": "Product Count Updated!!"});
+//             }
+//             db.close();
+//         });
+//     });
+// });
+
+app.post('/addStockTransaction', function (req, res) {
     MongoClient.connect(mongodbUrl, function(err, db) {
         if (err) throw err;        
         var dbo = db.db("stepp_db");
         dbo.collection("stock").insertOne({
+            "model_number": req["body"]["model_number"],
             "model_type": req["body"]["model_type"],
-            "model_number": req["body"]["model_number"],
+            "model_size": req["body"]["model_size"],
             "model_color": req["body"]["model_color"],
-            "available_count": req["body"]["available_count"]
-        }, 
-        function(err, result) {
-            if (err) throw err;
-            if(result["acknowledged"] == true) {
-                res.json({"response": "Product Added!!"});
-            }
-            db.close();
-        });
-    });
-});
-
-app.get('/getProducts', function (req, res) {
-    MongoClient.connect(mongodbUrl, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("stepp_db");
-        dbo.collection("stock").find({}).toArray(function(err, result) {
-            if (err) throw err;
-            res.json({"response": result});
-            db.close();
-        });
-    });
-});
-
-app.post('/addTransaction', function (req, res) {
-    MongoClient.connect(mongodbUrl, function(err, db) {
-        if (err) throw err;        
-        var dbo = db.db("stepp_db");
-        dbo.collection("transactions").insertOne({
-            "model_number": req["body"]["model_number"],
-            "transaction_type": req["body"]["transaction_type"],
-            "transaction_date": req["body"]["transaction_date"],
-            "product_rate": req["body"]["product_rate"],
-            "bill_number": req["body"]["bill_number"],
-            "product_quantity": req["body"]["product_quantity"],
+            "model_rate_per_piece": req["body"]["model_rate_per_piece"],
+            "txn_type": req["body"]["txn_type"],
+            "txn_income": req["body"]["txn_income"],
+            "txn_expense": req["body"]["txn_expense"],
+            "txn_date": req["body"]["txn_date"],
+            "txn_bill_no": req["body"]["txn_bill_no"],
+            "txn_quantity": req["body"]["txn_quantity"],
             "discounts": req["body"]["discounts"],
             "customization_cost": req["body"]["customization_cost"],
             "comments": req["body"]["comments"]
@@ -69,15 +114,21 @@ app.post('/addTransaction', function (req, res) {
     });
 });
 
-app.post('/updateProductCount', function (req, res) {
+app.post('/updateStockCount', function (req, res) {
     MongoClient.connect(mongodbUrl, function(err, db) {
         if (err) throw err;        
         var dbo = db.db("stepp_db");
-        dbo.collection("stock").updateOne({'model_number': req["body"]["model_number"]}, { $set: { 'available_count': req["body"]["available_count"]}}, 
+        dbo.collection("stock_available").insertOne({
+            "model_number": req["body"]["model_number"],
+            "model_type": req["body"]["model_type"],
+            "model_size": req["body"]["model_size"],
+            "model_color": req["body"]["model_color"],
+            "available_count": req["body"]["available_count"]
+        }, 
         function(err, result) {
             if (err) throw err;
             if(result["acknowledged"] == true) {
-                res.json({"response": "Product Count Updated!!"});
+                res.json({"response": "Stock count updated!!"});
             }
             db.close();
         });
