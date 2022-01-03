69a4d37a61450ee252571706c892517f0993343b
manickavivek <31manick@gmail.com>
2022-01-03 09:59:16 +0530

    - Added "add product" api on the node js app. - Added "add product" module on the front end app.

---
+++ 01_frontend_UI_app/scripts/addproduct.js
@@ -0,0 +1,65 @@
+$( document ).ready(function() {
+    genericApiCalls("GET", "/getSizes", "", getSizesSuccesscb, errorcb);
+    function getSizesSuccesscb(sizeData) {
+        console.log(sizeData);
+        let sizeOpt = "";
+        for(let size in sizeData["response"]) {
+            sizeOpt += "<option value='"+sizeData["response"][size]+"'>"+sizeData["response"][size]+"</option>";
+        }
+        $("#size").html(sizeOpt);
+        $("#loadingPopup").modal("hide");
+        setTimeout(function(){
+            genericApiCalls("GET", "/getColors", "", getColorsSuccesscb, errorcb);
+            function getColorsSuccesscb(colorData) {
+                console.log(colorData);
+                let colorOpt = "";
+                for(let color in colorData["response"]) {
+                    colorOpt += "<option value='"+colorData["response"][color]+"'>"+colorData["response"][color]+"</option>";
+                }
+                $("#color").html(colorOpt);
+                $("#loadingPopup").modal("hide");
+            }
+        }, 500);
+    }
+});
+
+function addProductBtnClick() {
+
+    let model = ($("#model").val()).trim();
+    let size = $("#size").val();
+    let color = $("#color").val();
+
+    if(model == "") {
+        $("#modelMsg").html("Enter all mandatory inputs!!");
+        $("#alertPopup").modal("show");
+        return;
+    }
+
+    let confirmation = confirm("Are you sure to add the product??");
+    if (confirmation == false) {
+        return;
+    }
+
+    let addPrdPayload = {};
+    addPrdPayload["model"] = model;
+    addPrdPayload["size"] = size;
+    addPrdPayload["color"] = color;
+
+    console.log("Calling addProduct API..");
+    genericApiCalls("POST", "/addProduct", addPrdPayload, addPrdSuccesscb, errorcb)
+
+    function addPrdSuccesscb(data) {
+        if(data["response"] == "Product Added!!" || data["response"] == "Product Already Exist!!") {
+            console.log(data["response"]);
+            $("#indexPgmodelMsg").html(data["response"]);
+            setTimeout(function(){
+                $("#indexPgRedirectionPopup").modal("show");
+            }, 500);
+        } else {
+            $("#modelMsg").html("Product Adding Failed!!");
+            setTimeout(function(){
+                $("#alertPopup").modal("show");
+            }, 500);
+        }
+    }
+}
--- 02_middleware_node_app/app.js
+++ 02_middleware_node_app/app.js
@@ -12,37 +12,58 @@ app.use(function(req, res, next) {
 
 app.use(express.json());
 
-// app.post('/addProduct', function (req, res) {
-//     MongoClient.connect(mongodbUrl, function(err, db) {
-//         if (err) throw err;        
-//         var dbo = db.db("stepp_db");
-//         dbo.collection("stock").insertOne({
-//             "model_type": req["body"]["model_type"],
-//             "model_number": req["body"]["model_number"],
-//             "model_color": req["body"]["model_color"],
-//             "available_count": req["body"]["available_count"]
-//         }, 
-//         function(err, result) {
-//             if (err) throw err;
-//             if(result["acknowledged"] == true) {
-//                 res.json({"response": "Product Added!!"});
-//             }
-//             db.close();
-//         });
-//     });
-// });
+app.post('/addProduct', function (req, res) {
+    MongoClient.connect(mongodbUrl, function(err, db) {
+        if (err) throw err;        
+        var dbo = db.db("stepp_db");
+        let proObj = {
+            "model": req["body"]["model"],
+            "size": req["body"]["size"],
+            "color": req["body"]["color"]
+        };
 
-// app.get('/getProducts', function (req, res) {
-//     MongoClient.connect(mongodbUrl, function(err, db) {
-//         if (err) throw err;
-//         var dbo = db.db("stepp_db");
-//         dbo.collection("stock").find({}).toArray(function(err, result) {
-//             if (err) throw err;
-//             res.json({"response": result});
-//             db.close();
-//         });
-//     });
-// });
+        dbo.collection("products").find(proObj).toArray(function(err, result) {
+            if (err) throw err;
+            if(result.length == 0) {
+                dbo.collection("products").insertOne(proObj,
+                function(err, result) {
+                    if (err) throw err;
+                    if(result["acknowledged"] == true) {
+                        res.json({"response": "Product Added!!"});
+                    }
+                    db.close();
+                });
+            } else {
+                res.json({"response": "Product Already Exist!!"});
+                db.close();
+            }
+        });
+    });
+});
+
+app.get('/getSizes', function (req, res) {
+    MongoClient.connect(mongodbUrl, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("stepp_db");
+        dbo.collection("raw_data").distinct("size", function(err, result) {
+            if (err) throw err;
+            res.json({"response": result});
+            db.close();
+        });
+    });
+});
+
+app.get('/getColors', function (req, res) {
+    MongoClient.connect(mongodbUrl, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("stepp_db");
+        dbo.collection("raw_data").distinct("color", function(err, result) {
+            if (err) throw err;
+            res.json({"response": result});
+            db.close();
+        });
+    });
+});
 
 // app.post('/addTransaction', function (req, res) {
 //     MongoClient.connect(mongodbUrl, function(err, db) {
@@ -84,105 +105,105 @@ app.use(express.json());
 //     });
 // });
 
-app.post('/addStockTransaction', function (req, res) {
-    MongoClient.connect(mongodbUrl, function(err, db) {
-        if (err) throw err;        
-        var dbo = db.db("stepp_db");
-        dbo.collection("stock").insertOne({
-            "model_number": req["body"]["model_number"],
-            "model_type": req["body"]["model_type"],
-            "model_size": req["body"]["model_size"],
-            "model_color": req["body"]["model_color"],
-            "model_rate_per_piece": req["body"]["model_rate_per_piece"],
-            "txn_type": req["body"]["txn_type"],
-            "txn_income": req["body"]["txn_income"],
-            "txn_expense": req["body"]["txn_expense"],
-            "txn_date": req["body"]["txn_date"],
-            "txn_bill_no": req["body"]["txn_bill_no"],
-            "txn_quantity": req["body"]["txn_quantity"],
-            "discounts": req["body"]["discounts"],
-            "customization_cost": req["body"]["customization_cost"],
-            "comments": req["body"]["comments"]
-        }, 
-        function(err, result) {
-            if (err) throw err;
-            if(result["acknowledged"] == true) {
-                res.json({"response": "Transaction Updated!!"});
-            }
-            db.close();
-        });
-    });
-});
+// app.post('/addStockTransaction', function (req, res) {
+//     MongoClient.connect(mongodbUrl, function(err, db) {
+//         if (err) throw err;        
+//         var dbo = db.db("stepp_db");
+//         dbo.collection("stock").insertOne({
+//             "model_number": req["body"]["model_number"],
+//             "model_type": req["body"]["model_type"],
+//             "model_size": req["body"]["model_size"],
+//             "model_color": req["body"]["model_color"],
+//             "model_rate_per_piece": req["body"]["model_rate_per_piece"],
+//             "txn_type": req["body"]["txn_type"],
+//             "txn_income": req["body"]["txn_income"],
+//             "txn_expense": req["body"]["txn_expense"],
+//             "txn_date": req["body"]["txn_date"],
+//             "txn_bill_no": req["body"]["txn_bill_no"],
+//             "txn_quantity": req["body"]["txn_quantity"],
+//             "discounts": req["body"]["discounts"],
+//             "customization_cost": req["body"]["customization_cost"],
+//             "comments": req["body"]["comments"]
+//         }, 
+//         function(err, result) {
+//             if (err) throw err;
+//             if(result["acknowledged"] == true) {
+//                 res.json({"response": "Transaction Updated!!"});
+//             }
+//             db.close();
+//         });
+//     });
+// });
 
-app.post('/updateStockCount', function (req, res) {
-    MongoClient.connect(mongodbUrl, function(err, db) {
-        if (err) throw err;
-        let dbo = db.db("stepp_db");
-        dbo.collection("stock_available").find({
-            "model_number": req["body"]["model_number"],
-            "model_type": req["body"]["model_type"],
-            "model_size": req["body"]["model_size"],
-            "model_color": req["body"]["model_color"]            
-        }).toArray(function(err, result) {
-            if (err) throw err;
-            //Based on the select statement add/update the collection
-            if(result.length == 0) {
-                //No entry is found and so add the model_number into "stock_available" collection
-                dbo.collection("stock_available").insertOne({
-                    "model_number": req["body"]["model_number"],
-                    "model_type": req["body"]["model_type"],
-                    "model_size": req["body"]["model_size"],
-                    "model_color": req["body"]["model_color"],
-                    "available_count": req["body"]["update_count"]
-                }, 
-                function(err, result) {
-                    if (err) throw err;
-                    if(result["acknowledged"] == true) {
-                        res.json({"response": "Stock count updated for new entry!!"});
-                    }
-                    db.close();
-                });
-            } else {
-                //Entry found and so update the model_number into "stock_available" collection
-                let updatedCount = 0;
-                let availableCountOnDb = parseInt(result[0]["available_count"]);
-                let updateType = req["body"]["update_type"];
-                let updateCount = parseInt(req["body"]["update_count"]);
-                if(updateType == "add") {
-                    updatedCount = availableCountOnDb + updateCount;
-                } else {
-                    updatedCount = availableCountOnDb - updateCount;
-                }
+// app.post('/updateStockCount', function (req, res) {
+//     MongoClient.connect(mongodbUrl, function(err, db) {
+//         if (err) throw err;
+//         let dbo = db.db("stepp_db");
+//         dbo.collection("stock_available").find({
+//             "model_number": req["body"]["model_number"],
+//             "model_type": req["body"]["model_type"],
+//             "model_size": req["body"]["model_size"],
+//             "model_color": req["body"]["model_color"]            
+//         }).toArray(function(err, result) {
+//             if (err) throw err;
+//             //Based on the select statement add/update the collection
+//             if(result.length == 0) {
+//                 //No entry is found and so add the model_number into "stock_available" collection
+//                 dbo.collection("stock_available").insertOne({
+//                     "model_number": req["body"]["model_number"],
+//                     "model_type": req["body"]["model_type"],
+//                     "model_size": req["body"]["model_size"],
+//                     "model_color": req["body"]["model_color"],
+//                     "available_count": req["body"]["update_count"]
+//                 }, 
+//                 function(err, result) {
+//                     if (err) throw err;
+//                     if(result["acknowledged"] == true) {
+//                         res.json({"response": "Stock count updated for new entry!!"});
+//                     }
+//                     db.close();
+//                 });
+//             } else {
+//                 //Entry found and so update the model_number into "stock_available" collection
+//                 let updatedCount = 0;
+//                 let availableCountOnDb = parseInt(result[0]["available_count"]);
+//                 let updateType = req["body"]["update_type"];
+//                 let updateCount = parseInt(req["body"]["update_count"]);
+//                 if(updateType == "add") {
+//                     updatedCount = availableCountOnDb + updateCount;
+//                 } else {
+//                     updatedCount = availableCountOnDb - updateCount;
+//                 }
                 
-                dbo.collection("stock_available").findOneAndUpdate({
-                    "model_number": req["body"]["model_number"],
-                    "model_type": req["body"]["model_type"],
-                    "model_size": req["body"]["model_size"],
-                    "model_color": req["body"]["model_color"]
-                }, {
-                    $set: {"available_count": updatedCount}
-                },
-                function(err, result) {
-                    if (err) throw err;
-                    res.json({"response": "Stock count updated for existing entry!!"});
-                    db.close();
-                });
-            }
-        });
-    });
-});
+//                 dbo.collection("stock_available").findOneAndUpdate({
+//                     "model_number": req["body"]["model_number"],
+//                     "model_type": req["body"]["model_type"],
+//                     "model_size": req["body"]["model_size"],
+//                     "model_color": req["body"]["model_color"]
+//                 }, {
+//                     $set: {"available_count": updatedCount}
+//                 },
+//                 function(err, result) {
+//                     if (err) throw err;
+//                     res.json({"response": "Stock count updated for existing entry!!"});
+//                     db.close();
+//                 });
+//             }
+//         });
+//     });
+// });
 
-app.get('/getAvailableStocks', function (req, res) {
-    MongoClient.connect(mongodbUrl, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("stepp_db");
-        dbo.collection("stock_available").find({}).toArray(function(err, result) {
-            if (err) throw err;
-            res.json({"response": result});
-            db.close();
-        });
-    });
-});
+// app.get('/getAvailableStocks', function (req, res) {
+//     MongoClient.connect(mongodbUrl, function(err, db) {
+//         if (err) throw err;
+//         var dbo = db.db("stepp_db");
+//         dbo.collection("stock_available").find({}).toArray(function(err, result) {
+//             if (err) throw err;
+//             res.json({"response": result});
+//             db.close();
+//         });
+//     });
+// });
 
 app.listen(3000, function () {
   console.log('Stepp app listening on port 3000!');
